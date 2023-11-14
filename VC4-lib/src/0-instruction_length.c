e2b2d885fe287c694e11142a6c1093e514443fc3
Wyatt <105826584+wwhitfi7@users.noreply.github.com>
2023-11-14 01:31:24 -0500

    Fixed the clangd issue, encountered a new one but we're now cooking with peanut oil
    Signed-off-by: Wyatt <105826584+wwhitfi7@users.noreply.github.com>

--- VC4-lib/src/0-instruction_length.c
+++ VC4-lib/src/0-instruction_length.c
@@ -5,20 +5,20 @@ unsigned short next_instruction_is(unsigned char* input)
         unsigned char val = *input & 0xF8 ;
         switch (val) {
                 case 0xF8:
-                    return VECTOR80;
+                    return 80;
                 case 0xF0:
-                    return VECTOR48;
+                    return 48;
         }
         val = val & 0xF0;
         switch (val) {
                 case 0xE0:
-                    return SCALAR48;
+                    return 48;
         }
         val = val & 0x80;
         switch (val) {
                 case 0x80:
-                    return SCALAR32;
+                    return 32;
                 case 0x00:
-                    return SCALAR16;
+                    return 16;
         }
 }
