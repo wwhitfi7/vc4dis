58d273315f931cf14918fde17437859c4ed94876
Wyatt <105826584+wwhitfi7@users.noreply.github.com>
2023-12-05 02:28:14 -0500

    feat: complete refactor of structure Realized targeted hardware has some specialized features that don't generalize well. Refactored to only target a singular VPU. Will expand to generalize if necessary much further down the line.

---
+++ disassembler/inc/definitions.h
@@ -0,0 +1,87 @@
+/*
+
+MIT License
+
+Copyright (c) 2023 Wyatt Whitfield
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+// Includes
+#include <cstdint>
+
+// Provides definitions of common data types in this program - instructions,
+// arguments, etc.
+
+// Enumeration of possible instructions
+enum instruction_type {
+    bkpt,
+    halt, 
+    sleep,
+    user,
+    enable_interrupts,
+    disable_interrupts,
+    col_base_clear,
+    col_base_add_1,
+    col_base_add_2,
+    col_base_add_3,
+    return_from_interrupt,
+    software_interrupt,
+    branch,
+    branch_long,
+    switch_b,
+    switch_s,
+    version,
+    load_from_stack,
+    store_to_stack,
+    load,
+    store,
+    add_offset,
+    branch_conditional,
+    math_logic_op,
+    add_cond_branch,
+    math_logic_cond,
+    fl_op,
+    ftrunc_cond,
+    floor,
+    flts,
+    fltu,
+    proc_control_reg_mov,
+    jump,
+    jump_long,
+    vector_op
+};
+
+// Typedefs here
+typedef enum instruction_type instruction_type;
+typedef struct instruction instruction;
+typedef struct arg arg;
+
+struct arg {
+
+};
+
+// Struct defs here
+struct instruction {
+    instruction_type type;
+    uint8_t *contents; // location of instruction in source binary's buffer
+    char *name; // Null-terminated c-string naming the instruction
+    arg *args; // Array of args
+};
